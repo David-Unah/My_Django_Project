a536877499c5c78380a8c9c1b6bb162ffe8a95f3
David Unah <davidunah022@gmail.com>
2022-11-16 10:42:56 +0100

    Updated models

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -7,10 +7,26 @@ class Artiste(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+
+
 class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=40)
-    date_released = models.DateField()
+    date_released = models.DateField(default=datetime.today)
     artiste_id = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
 class Lyrics(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.CharField(max_length=400)
     song_id = models.IntegerField()
+
+    def __str__(self):
+        return self.content
